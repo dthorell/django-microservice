fdaec8563d546ce8422190fe413a6b56b46beab4
Daniel Thorell <dthorell@traveller.nu>
2021-09-07 16:00:42 +0200

    Reformatted with black, add back in requirements for admin

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,8 +1,13 @@
+# from django.contrib import admin   # Uncomment if using admin
 from rest_framework import routers
+
 # Import your viewsets
 
 router = routers.DefaultRouter()
 # Register your ViewSets
 
+urlpatterns = [
+    # path("admin/", admin.site.urls),  # Uncomment if using admin
+]
 
-urlpatterns = router.urls
+urlpatterns += router.urls
